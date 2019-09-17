85d7ee43094abaa051bb4acd236ae88671be5ae7
kilbot <paul@kilbot.com.au>
2019-09-17 18:38:41 +0300

    useData (wip)

---
+++ src/hooks/use-data/use-data.ts
@@ -0,0 +1,51 @@
+import { Q } from '@nozbe/watermelondb';
+import useDatabase from '../use-database';
+import useObservable from '../use-observable';
+import useApi from '../use-api';
+
+function initialFetch(type) {
+	const { storeDB } = useDatabase();
+
+	const api = useApi(type);
+
+	const collection = storeDB.collections.get(type);
+	if (api.data) {
+		debugger;
+		const batch = api.data.map((json: any) => {
+			return collection.prepareCreate((model: Product) => {
+				Object.keys(json).forEach((key: string) => {
+					switch (key) {
+						case 'id':
+							model.remote_id = json.id;
+							break;
+						default:
+							// @ts-ignore
+							model[key] = json[key];
+					}
+				});
+			});
+		});
+		storeDB.action(async () => await storeDB.batch(...batch));
+	}
+}
+
+function useData(type, search = '') {
+	const { storeDB } = useDatabase();
+
+	const data = useObservable(
+		storeDB.collections
+			.get(type)
+			.query(Q.where('name', Q.like(`%${Q.sanitizeLikeString(search)}%`)))
+			.observeWithColumns(['name', 'regular_price']),
+		[]
+	);
+
+	// initial fetch
+	// if (data.length === 0) {
+	// 	initialFetch(type);
+	// }
+
+	return { data };
+}
+
+export default useData;
