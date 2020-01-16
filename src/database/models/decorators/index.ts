e720866d631651c98f492c3b50caaa481eb4ce13
kilbot <paul@kilbot.com.au>
2020-01-17 02:15:06 +0400

    woocommerce service

--- src/database/models/decorators/index.ts
+++ src/database/models/decorators/index.ts
@@ -1,7 +1,8 @@
 import address from './address';
 import children from './children';
 import date from './date';
+import field from './field';
 import pivot from './pivot';
 import relation from './relation';
 
-export { address, children, date, pivot, relation };
+export { address, children, date, field, pivot, relation };
