bda491adb4582a80177e7eef68a13d3e5fe8d70c
kilbot <paul@kilbot.com.au>
2020-06-12 23:07:05 +0400

    useAppState hook

--- src/hooks/use-app-state/app-state-provider.tsx
+++ src/hooks/use-app-state/app-state-provider.tsx
@@ -1,43 +1,70 @@
 import React from 'react';
 import { Dimensions } from 'react-native';
+import { getUniqueId, getReadableVersion } from './device-info';
 import useNetInfo from './use-net-info';
 import useDimensions from './use-dimensions';
-import * as consts from './consts';
+import useDatabase from './use-database';
+import * as actionTypes from './action-types';
 
-const initialState = {
-	online: false, // optimistic?
-	window: Dimensions.get('window'),
-	screen: Dimensions.get('screen'),
+export type AppState = {
+	online: boolean;
+	window: import('react-native').ScaledSize;
+	screen: import('react-native').ScaledSize;
+	info: {
+		uniqueId: string;
+		version: string;
+	};
+	user?: any;
+	storeDB?: any;
+	site?: any;
+	wpUser?: any;
+};
+export type ActionTypes = typeof actionTypes;
+export type AppAction = {
+	type: Extract<keyof typeof actionTypes, string>;
+	payload?: any;
 };
 
-export type AppState = typeof initialState;
-export type AppAction = {
-	type: Extract<keyof typeof consts, string>;
-	payload: any;
+const initialState: AppState = {
+	online: false,
+	window: Dimensions.get('window'),
+	screen: Dimensions.get('screen'),
+	info: {
+		uniqueId: getUniqueId(),
+		version: getReadableVersion(),
+	},
+	user: undefined,
+	storeDB: undefined,
+	site: undefined,
+	wpUser: undefined,
 };
 
 function appStateReducer(state: AppState, action: AppAction): AppState {
 	const { type, payload } = action;
+	console.log(type, payload);
 	switch (type) {
-		case consts.DIMENSIONS_CHANGE:
-			return { ...state, ...payload };
-		case consts.IS_ONLINE:
-			return { ...state, ...payload };
+		// case consts.DIMENSIONS_CHANGE:
+		// 	return { ...state, ...payload };
+		// case consts.IS_ONLINE:
+		// 	return { ...state, ...payload };
 		// case SET_THEME:
 		// 	return { ...state, colorTheme: action.theme };
 		default:
-			return state;
+			return { ...state, ...payload };
 	}
 }
 
-export const AppStateContext = React.createContext<[AppState, React.Dispatch<AppAction>]>(null);
+export const AppStateContext = React.createContext<
+	[AppState, React.Dispatch<AppAction>, ActionTypes]
+>(null);
 
 const AppStateProvider: React.FC = ({ children }) => {
 	const [state, dispatch] = React.useReducer(appStateReducer, initialState);
-	const value = React.useMemo(() => [state, dispatch], [state]) as any;
+	const value = React.useMemo(() => [state, dispatch, actionTypes], [state]) as any;
 
 	useNetInfo(state, dispatch);
 	useDimensions(dispatch);
+	useDatabase(dispatch);
 
 	return <AppStateContext.Provider value={value}>{children}</AppStateContext.Provider>;
 };
