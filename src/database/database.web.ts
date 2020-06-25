120fd059bb9dff946efca5a7d615f47bd67f3ef6
kilbot <paul@kilbot.com.au>
2020-06-25 21:43:31 +0400

    transition db to rxdb (wip)

--- src/database/database.web.ts
+++ src/database/database.web.ts
@@ -1,6 +1,37 @@
 import { createRxDatabase, addRxPlugin } from 'rxdb';
 import idbAdapter from 'pouchdb-adapter-idb';
 import schema from './user/app-user-schema.json';
+import sitesSchema from './user/sites-schema.json';
+
+type AppUser = {
+	id: string;
+	first_name: string;
+	last_name: string;
+	display_name: string;
+	sites: [];
+};
+
+const docMethods = {
+	async addSite(url) {
+		const trimmedUrl = url.replace(/^.*:\/{2,}|\s|\/+$/g, '');
+		if (trimmedUrl) {
+			const newSite = await this.collection.database.collections.sites.upsert({
+				id: 'site-0',
+				url: trimmedUrl,
+			});
+			await this.update({ $set: { sites: [newSite.id] } });
+			console.log(this);
+		}
+	},
+};
+
+const colMethods = {
+	async createNewUser() {
+		return this.insert({ id: 'new-0', first_name: 'Default', last_name: 'User' });
+	},
+};
+
+type AppUserCollection = import('rxdb').RxCollection<AppUser, typeof docMethods, typeof colMethods>;
 
 addRxPlugin(idbAdapter);
 
@@ -14,6 +45,15 @@ const getDatabase = async (name: string) => {
 		await db.collection({
 			name: 'app_users',
 			schema,
+			methods: docMethods,
+			statics: colMethods,
+			options: {
+				foo: 'bar',
+			},
+		});
+		await db.collection({
+			name: 'sites',
+			schema: sitesSchema,
 		});
 	}
 	return db;
