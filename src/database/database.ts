179cdb41a60b80775320830cb139481c1e699bac
kilbot <paul@kilbot.com.au>
2020-06-30 22:44:38 +0400

    add types

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,15 @@
 import createDatabase from './adapter';
 import createCollectionMap from './users';
 
-const databasePromise = createDatabase('wcpos_users').then((db) => {
+type Collections = {
+	logs: import('./users/logs/logs').LogsCollection;
+	// eslint-disable-next-line camelcase
+	app_users: import('./users/app-users/app-users').AppUsersCollection;
+	sites: import('./users/sites/sites').SitesCollection;
+};
+export type Database = import('rxdb').RxDatabase<Collections>;
+
+const databasePromise: Promise<Database> = createDatabase('wcpos_users').then((db) => {
 	return Promise.all(
 		createCollectionMap.map((createCollection) => {
 			return createCollection(db);
