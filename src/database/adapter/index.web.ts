4434515295abcd5cef103fd128601945f0479ac0
kilbot <paul@kilbot.com.au>
2020-07-01 19:25:39 +0400

    add rxdb/utils

--- src/database/adapter/index.web.ts
+++ src/database/adapter/index.web.ts
@@ -1,10 +1,12 @@
 import { createRxDatabase, addRxPlugin } from 'rxdb';
 import idbAdapter from 'pouchdb-adapter-idb';
+// import { models } from 'rxdb-utils';
 
 type Collections = import('../database').Collections;
 type Database = import('../database').Database;
 
 addRxPlugin(idbAdapter);
+// addRxPlugin(models);
 
 const getDatabase = async (name: string): Promise<Database> => {
 	const db = createRxDatabase<Collections>({
