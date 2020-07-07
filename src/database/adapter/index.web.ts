8b838b4218f7ddcd6015ad5e85e79b6c8afe8d3e
kilbot <paul@kilbot.com.au>
2020-07-07 15:03:23 +0400

    update product schema

--- src/database/adapter/index.web.ts
+++ src/database/adapter/index.web.ts
@@ -1,12 +1,27 @@
-import { createRxDatabase, addRxPlugin } from 'rxdb';
+import { createRxDatabase, addRxPlugin } from 'rxdb/plugins/core';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import idbAdapter from 'pouchdb-adapter-idb';
+import { RxDBValidatePlugin } from 'rxdb/plugins/validate';
+import { RxDBQueryBuilderPlugin } from 'rxdb/plugins/query-builder';
+import { RxDBLocalDocumentsPlugin } from 'rxdb/plugins/local-documents';
+import { RxDBUpdatePlugin } from 'rxdb/plugins/update';
 import collections from 'rxdb-utils/dist/collections';
+import wcPlugin from '../plugins/woocommerce';
 
 type Collections = import('../database').Collections;
 type Database = import('../database').Database;
 
+if (process.env.NODE_ENV !== 'production') {
+	addRxPlugin(RxDBDevModePlugin);
+}
+
 addRxPlugin(idbAdapter);
+addRxPlugin(RxDBValidatePlugin);
+addRxPlugin(RxDBLocalDocumentsPlugin);
+addRxPlugin(RxDBQueryBuilderPlugin);
+addRxPlugin(RxDBUpdatePlugin);
 addRxPlugin(collections);
+addRxPlugin(wcPlugin);
 
 const getDatabase = async (name: string): Promise<Database> => {
 	const db = createRxDatabase<Collections>({
--- src/database/stores/products/products.ts
+++ src/database/stores/products/products.ts
@@ -1,6 +1,8 @@
 import { ObservableResource } from 'observable-hooks';
 import { from } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
+import difference from 'lodash/difference';
+import unset from 'lodash/unset';
 import schema from './schema.json';
 
 export type Schema = import('./interface').WooCommercePOSStoreSchema;
@@ -38,6 +40,20 @@ const createProductsCollection = async (db: Database): Promise<Collection> => {
 		methods,
 		statics,
 	});
+
+	// @TODO - turn this into a plugin?
+	ProductsCollection.preInsert(function (rawData) {
+		// remove _links property (invalid property name)
+		unset(rawData, '_links');
+		// remove propeties not on schema
+		const omitProperties = difference(Object.keys(rawData), this.schema.topLevelFields);
+		console.log('the following properties are being omiited', omitProperties);
+		omitProperties.forEach((prop) => {
+			unset(rawData, prop);
+		});
+		// change id to string
+		rawData.id = String(rawData.id);
+	}, false);
 
 	// ProductsCollection.postCreate((raw, model) => {
 	// 	const dbResource = new ObservableResource(
