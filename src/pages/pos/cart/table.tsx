dfc49d697bd6690b3a080f8253465dafb616a31a
kilbot <paul@kilbot.com.au>
2020-07-16 12:26:18 +0400

    react to quantity changes

--- src/pages/pos/cart/table.tsx
+++ src/pages/pos/cart/table.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import { useObservable, useObservableState } from 'observable-hooks';
-import { from } from 'rxjs';
-import { switchMap, tap, catchError, map } from 'rxjs/operators';
+import { from, of, combineLatest } from 'rxjs';
+import { switchMap, tap, catchError, map, switchMapTo } from 'rxjs/operators';
 import { useTranslation } from 'react-i18next';
 import Table from '../../../components/table';
 import Text from '../../../components/text';
+import TextInput from '../../../components/textinput';
 
 const CartTable = ({ columns, order }) => {
 	const { t } = useTranslation();
 
-	const lineItems$ = useObservable(() =>
-		order.line_items$.pipe(
-			switchMap((ids) => from(order.collections().line_items.findByIds(ids))),
-			map((result) => Array.from(result.values())),
-			catchError((err) => console.error(err))
-		)
+	const lineItems$ = order.line_items$.pipe(
+		switchMap((ids) => from(order.collections().line_items.findByIds(ids))),
+		map((result) => Array.from(result.values())),
+		switchMap((array) => combineLatest(array.map((item) => item.$))),
+		// super hacky ... I want to return the document, not the change event
+		switchMap((array) =>
+			combineLatest(array.map((item) => order.collections().line_items.findOne(item.id).$))
+		),
+		tap((res) => console.log(res)),
+		catchError((err) => console.error(err))
 	);
 
-	const lineItems = useObservableState(lineItems$, []);
+	const [lineItems] = useObservableState(() => lineItems$, []);
 
 	const renderCell = ({ getCellProps }) => {
 		const { cellData, column, rowData } = getCellProps();
 		let children;
 
 		switch (column.key) {
+			case 'quantity':
+				children = (
+					<TextInput
+						value={cellData}
+						onChangeText={async (val) => {
+							rowData.update({
+								$set: {
+									quantity: Number(val),
+								},
+							});
+						}}
+					/>
+				);
+				break;
 			case 'total':
 				children = <Text>{rowData.total}</Text>;
 				break;
