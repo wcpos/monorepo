cea15118d29aa17bce14cd08d65be4555a56c4f0
kilbot <paul@kilbot.com.au>
2021-04-08 11:02:16 +0200

    add products page

--- src/hooks/use-ui/use-ui.ts
+++ src/hooks/use-ui/use-ui.ts
@@ -4,26 +4,35 @@ import { ObservableResource } from 'observable-hooks';
 import useAppState from '../use-app-state';
 import initialUI from './ui-initial.json';
 
-type UIKeys = 'posProducts' | 'posCart';
+interface UISchema {
+	sortBy: string;
+	sortDirection: import('@wcpos/common/src/components/table/types').SortDirection;
+	width: string;
+	columns: [{ key: string; disableSort: boolean; order: number }];
+	display: [{ key: string; hide: boolean; order: number }];
+}
+
 type StoreDatabase = import('@wcpos/common/src/database').StoreDatabase;
-type UIDocument = Record<string, unknown>;
+type UIDocument = import('rxdb').RxDocument<UISchema>;
 type UIResource = import('observable-hooks').ObservableResource<UIDocument>;
 
 interface IUIResources {
 	posProducts?: UIResource;
 	posCart?: UIResource;
+	products?: UIResource;
 }
 
 const uiResources: IUIResources = {
 	posProducts: undefined,
 	posCart: undefined,
+	products: undefined,
 };
 
 /**
  *
  * @param key
  */
-export const useUI = (key: UIKeys): UIResource => {
+export const useUI = (key: Extract<keyof IUIResources, string>) => {
 	const { storeDB } = useAppState() as { storeDB: StoreDatabase };
 
 	if (uiResources[key]) {
