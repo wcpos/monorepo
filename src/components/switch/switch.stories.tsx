1f4e50dc4040904216503ea3c05cfde78f78d2c6
kilbot <paul@kilbot.com.au>
2021-05-07 21:36:14 +0200

    add switch component

---
+++ src/components/switch/switch.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Switch, SwitchProps } from './switch';
+
+export default {
+	title: 'Components/Switch',
+	component: Switch,
+};
+
+export const BasicUsage = (props: SwitchProps) => <Switch {...props} />;
