47a9bed41ce52a44dea1d0d724bd769ed8a2c61e
kilbot <paul@kilbot.com.au>
2021-05-26 13:53:36 +0200

    fix typos

--- src/database/collections/shipping-lines/statics.ts
+++ src/database/collections/shipping-lines/statics.ts
@@ -20,7 +20,7 @@ export default {
 			// @ts-ignore
 			const _id = get(find(item.meta_data, { key: '_pos' }), 'value');
 			// @ts-ignore
-			const upsertedItem = this.collections().line_items.upsert({
+			const upsertedItem = this.collections().shipping_lines.upsert({
 				_id,
 				...(item as Record<string, unknown>),
 			});
