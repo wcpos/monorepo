e20c4d795a171cb07f27f4633447e39729a53df2
kilbot <paul@kilbot.com.au>
2021-06-27 17:43:07 +0200

    delete wpUser from POS

--- src/screens/auth/auth.tsx
+++ src/screens/auth/auth.tsx
@@ -51,6 +51,7 @@ const Auth = () => {
 					onAction={onConnect}
 					type="url"
 					clearable
+					hideLabel
 				/>
 			</Segment>
 			{sites && sites.length > 0 && (
--- src/screens/auth/wp-user.tsx
+++ src/screens/auth/wp-user.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
 import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
+import forEach from 'lodash/forEach';
 import Tag from '@wcpos/common/src/components/tag';
+import Dialog from '@wcpos/common/src/components/dialog';
+import Text from '@wcpos/common/src/components/text';
 import useAppState from '@wcpos/common/src/hooks/use-app-state';
+import DatabaseService from '@wcpos/common/src/database';
 
 interface Props {
 	site: import('@wcpos/common/src/database').SiteDocument;
 	wpUser: import('@wcpos/common/src/database').WPCredentialsDocument;
 }
 
+function sanitizeStoreName(id: string) {
+	return `store_${id.replace(':', '_')}`;
+}
+
 const WpUser = ({ site, wpUser }: Props) => {
 	const { setLastUser } = useAppState();
+	const [showDialog, setShowDialog] = React.useState(false);
+	// const [stores, setStores] = React.useState([]);
+
+	// 	/**
+	//  * Populate stores on first render
+	//  */
+	// React.useEffect(() => {}, [])
 
 	/**
 	 *
@@ -32,14 +47,58 @@ const WpUser = ({ site, wpUser }: Props) => {
 	/**
 	 *
 	 */
-	const handleWpUserRemove = React.useCallback(async () => {
-		console.log(wpUser);
-	}, []);
+	const handleStoreRemove = React.useCallback(async () => {
+		forEach(wpUser.stores, async (id) => {
+			// @ts-ignore
+			const db = await DatabaseService.getStoreDB(sanitizeStoreName(id));
+			await db?.remove();
+		});
+		// need to remove wpUser from site from site
+		await site.update({
+			$pullAll: {
+				wpCredentials: [wpUser._id],
+			},
+		});
+		await wpUser.remove();
+
+		setShowDialog(false);
+	}, [site, wpUser]);
+
+	/**
+	 *
+	 */
+	const handleWpUserRemove = React.useCallback(() => {
+		setShowDialog(true);
+	}, [setShowDialog]);
 
 	return (
-		<Tag removable onPress={handleStoreSelect} onRemove={handleWpUserRemove}>
-			{wpUser.displayName ? wpUser.displayName : 'No name?'}
-		</Tag>
+		<>
+			<Tag removable onPress={handleStoreSelect} onRemove={handleWpUserRemove}>
+				{wpUser.displayName ? wpUser.displayName : 'No name?'}
+			</Tag>
+			{showDialog && (
+				<Dialog
+					title="Delete User"
+					open
+					onClose={() => {
+						setShowDialog(false);
+					}}
+					primaryAction={{ label: 'Confirm', action: handleStoreRemove, type: 'critical' }}
+					secondaryActions={[
+						{
+							label: 'Cancel',
+							action: () => {
+								setShowDialog(false);
+							},
+						},
+					]}
+				>
+					<Dialog.Section>
+						<Text>You are about to remove all stores associated with this user</Text>
+					</Dialog.Section>
+				</Dialog>
+			)}
+		</>
 	);
 };
 
