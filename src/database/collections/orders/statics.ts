20d7a814409e9fc5bf0a5a4e9f27e392f7b83095
kilbot <paul@kilbot.com.au>
2021-07-04 14:00:01 +0200

    add useDataObservable hook

--- src/database/collections/orders/statics.ts
+++ src/database/collections/orders/statics.ts
@@ -1,11 +1,99 @@
+import _filter from 'lodash/filter';
+import _sortBy from 'lodash/sortBy';
+import _map from 'lodash/map';
+
 type OrderCollection = import('.').OrderCollection;
 type ProductDocument = import('../products').ProductDocument;
 type ProductVariationDocument = import('../product-variations').ProductVariationDocument;
+type QueryState =
+	import('@wcpos/common/src/hooks/use-data-observable/use-data-observable').QueryState;
 
 /**
  * WooCommerce Order Collection statics
  */
 export default {
+	/**
+	 *
+	 */
+	query(this: OrderCollection, query: QueryState) {
+		return this.find();
+	},
+
+	/**
+	 *
+	 */
+	restApiQuery(this: OrderCollection, query: QueryState) {
+		this.pouch
+			.find({
+				selector: {},
+				// @ts-ignore
+				fields: ['_id', 'id', 'dateCreatedGmt'],
+			})
+			.then((result: any) => {
+				// get array of sorted records with dateCreatedGmt
+				const filtered = _filter(result.docs, 'dateCreatedGmt');
+				const sorted = _sortBy(filtered, 'dateCreatedGmt');
+				const exclude = _map(sorted, 'id').join(',');
+
+				// @ts-ignore
+				const replicationState = this.syncRestApi({
+					live: false,
+					autoStart: false,
+					pull: {
+						queryBuilder: (lastModified: any) => {
+							return {
+								search: query.search,
+								order: query.sortDirection,
+								orderBy: query.sortBy,
+								exclude,
+							};
+							// const orderbyMap = {
+							// 	lastName: 'meta_value',
+							// 	firstName: 'meta_value',
+							// };
+
+							// const metaKeyMap = {
+							// 	lastName: 'last_name',
+							// 	firstName: 'first_name',
+							// };
+
+							// // @ts-ignore
+							// const orderby = orderbyMap[query.sortBy] ? orderbyMap[query.sortBy] : query.sortBy;
+							// // @ts-ignore
+							// const meta_key = metaKeyMap[query.sortBy] ? metaKeyMap[query.sortBy] : undefined;
+
+							// return {
+							// 	search: escape(query.search),
+							// 	order: query.sortDirection,
+							// 	orderby,
+							// 	exclude,
+							// 	meta_key,
+							// };
+						},
+					},
+				});
+
+				replicationState.run(false);
+			})
+			.catch((err: any) => {
+				console.log(err);
+			});
+	},
+
+	/**
+	 *
+	 */
+	audit(this: OrderCollection) {
+		return this.database.httpClient
+			.get('orders', {
+				params: { fields: ['id'], posts_per_page: -1 },
+			})
+			.then(({ data }: any) => {
+				// @ts-ignore
+				return this.auditIdsFromServer(data);
+			});
+	},
+
 	/**
 	 *
 	 */
