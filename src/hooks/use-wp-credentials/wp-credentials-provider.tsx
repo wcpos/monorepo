e3436a9f40c17a1642eb9404e538395d9ae140fb
kilbot <paul@kilbot.com.au>
2021-10-09 13:39:41 +0200

    refactor app init
    WIP

--- src/hooks/use-wp-credentials/wp-credentials-provider.tsx
+++ src/hooks/use-wp-credentials/wp-credentials-provider.tsx
@@ -9,6 +9,7 @@ type WPCredentialsDocument = import('@wcpos/common/src/database').WPCredentialsD
 
 interface WpCredentialsContextProps {
 	wpCredentialsResource: ObservableResource<WPCredentialsDocument>;
+	wpUsersResource: ObservableResource<WPCredentialsDocument[]>;
 }
 
 // @ts-ignore
@@ -54,9 +55,22 @@ const WpCredentialsProvider = ({
 
 	const wpCredentialsResource = new ObservableResource(wpCredentials$);
 
+	// all wpCredentials for current site
+	const wpUsers$ = site.wpCredentials$.pipe(
+		switchMap((wpCredentialsIds: string[]) => {
+			return userDB.wp_credentials.findByIds$(wpCredentialsIds || []);
+		}),
+		map((m) => {
+			// @ts-ignore
+			return Array.from(m.values());
+		})
+	);
+
+	const wpUsersResource = new ObservableResource(wpUsers$);
+
 	return (
 		// @ts-ignore
-		<WpCredentialsContext.Provider value={{ wpCredentialsResource }}>
+		<WpCredentialsContext.Provider value={{ wpCredentialsResource, wpUsersResource }}>
 			{children}
 		</WpCredentialsContext.Provider>
 	);
