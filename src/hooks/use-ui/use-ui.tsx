e35f5733a44a05321dd307439e9d29b9dd2abe4c
kilbot <paul@kilbot.com.au>
2021-10-12 13:44:36 +0200

    refactor useUIResources

---
+++ src/hooks/use-ui/use-ui.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { UIResourceContext } from './ui-resource-provider';
+
+const useUI = () => {
+	const context = React.useContext(UIResourceContext);
+	if (context === undefined) {
+		throw new Error(`useUI must be called within UIResourceProvider`);
+	}
+
+	return context;
+}
+
+export default useUI;
