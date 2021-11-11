e2156683204a6db738d432a2e22956f1f82cebfd
kilbot <paul@kilbot.com.au>
2021-11-11 14:48:35 +0100

    add craco app for web dev

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
-import { Paragraph, Strong } from "@wcpos/ui";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+// import Text from "@wcpos/common/src/components/text";
+// import { Paragraph, Strong } from "@wcpos/ui";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Paragraph>
-        Hello from an <Strong>EAS</Strong> monorepo (managed)
-      </Paragraph>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Paragraph>
+//         Hello from an <Strong>EAS</Strong> monorepo (managed)
+//       </Paragraph>
+//       <Text>Hello from @wcpos/common</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+export { default } from "@wcpos/common/src/app";
