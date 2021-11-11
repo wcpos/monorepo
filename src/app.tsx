3896ff6dfdabb17990b347fcca94ecb64ab0b361
kilbot <paul@kilbot.com.au>
2021-11-11 16:51:21 +0100

    Testing app.tsx (WIP)

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,16 @@
-import * as React from "react";
-import { Text } from "react-native";
+import * as React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ErrorBoundary from './components/error-boundary';
 
-const App = () => <Text>Hello, World!</Text>;
+const App = () => {
+	return (
+		<React.Suspense fallback={<Text>loading app...</Text>}>
+			<SafeAreaProvider style={{ overflow: 'hidden' }}>
+				<Text>Hello, World!</Text>
+			</SafeAreaProvider>
+		</React.Suspense>
+	);
+};
 
 export default App;
