924d85a0d450aeb7b6690498e4c55e395a47bdf6
kilbot <paul@kilbot.com.au>
2021-11-17 12:37:52 +0100

    Update image.tsx

--- src/screens/pos/products/cells/image.tsx
+++ src/screens/pos/products/cells/image.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Image as RNImage } from 'react-native';
+import { Image as RNImage, View } from 'react-native';
 // import Img from '@wcpos/common/src/components/image';
+import Skeleton from '@wcpos/common/src/components/skeleton';
 
 type Props = {
 	item: import('@wcpos/common/src/database').ProductDocument;
@@ -8,13 +9,25 @@ type Props = {
 
 const Image = ({ item: product }: Props) => {
 	const { thumbnail } = product;
+	const [size, setSize] = React.useState({ width: undefined, height: undefined });
+
+	const onLayout = React.useCallback((event) => {
+		const { width, height } = event.nativeEvent.layout;
+		setSize({ width, height });
+	}, []);
 
 	return (
-		<RNImage
-			source={{ uri: thumbnail }}
-			style={{ width: '100%', height: undefined, aspectRatio: 1 }}
-			// placeholder={<Img source={require('@wcpos/common/src/assets/placeholder.png')} />}
-		/>
+		<View onLayout={onLayout} style={{ width: '100%' }}>
+			{thumbnail ? (
+				<RNImage
+					source={{ uri: thumbnail }}
+					style={{ width: size.width, height: size.width, aspectRatio: 1 }}
+					// placeholder={<Img source={require('@wcpos/common/src/assets/placeholder.png')} />}
+				/>
+			) : (
+				<Skeleton style={{ width: size.width, height: size.width }} />
+			)}
+		</View>
 	);
 };
 
