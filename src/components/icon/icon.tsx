038ccbdcce413902e9064bc0ae5ff3eb4a3f194a
kilbot <paul@kilbot.com.au>
2021-11-29 20:54:48 +0100

    tweak icons

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ViewStyle } from 'react-native';
 import get from 'lodash/get';
 import { useTheme } from 'styled-components/native';
-import Svgs from './svg/fontawesome/solid';
+import * as Svgs from './svg/fontawesome/solid';
 import Tooltip from '../tooltip2';
 import Skeleton from '../skeleton';
 import * as Styled from './styles';
--- src/components/icon/svg/fontawesome/solid/index.ts
+++ src/components/icon/svg/fontawesome/solid/index.ts
@@ -1,127 +1,62 @@
-import addressCard from './address-card.svg';
-import arrowRotateLeft from './arrow-rotate-left.svg';
-import arrowRotateRight from './arrow-rotate-right.svg';
-import badgeDollar from './badge-dollar.svg';
-import badgePercent from './badge-percent.svg';
-import ban from './ban.svg';
-import barcode from './barcode.svg';
-import basketShopping from './basket-shopping.svg';
-import bell from './bell.svg';
-import bookmark from './bookmark.svg';
-import boxOpenFull from './box-open-full.svg';
-import boxesStacked from './boxes-stacked.svg';
-import calculatorSimple from './calculator-simple.svg';
-import calculator from './calculator.svg';
-import calendarDays from './calendar-days.svg';
-import calendar from './calendar.svg';
-import cartPlus from './cart-plus.svg';
-import cartShopping from './cart-shopping.svg';
-import cashRegister from './cash-register.svg';
-import check from './check.svg';
-import circleCheck from './circle-check.svg';
-import circleChevronRight from './circle-chevron-right.svg';
-import circleExclamation from './circle-exclamation.svg';
-import circleInfo from './circle-info.svg';
-import circleMinus from './circle-minus.svg';
-import circlePause from './circle-pause.svg';
-import circlePlus from './circle-plus.svg';
-import circleQuestion from './circle-question.svg';
-import circleXmark from './circle-xmark.svg';
-import circle from './circle.svg';
-import clock from './clock.svg';
-import commentsQuestion from './comments-question.svg';
-import creditCard from './credit-card.svg';
-import ellipsisVertical from './ellipsis-vertical.svg';
-import folders from './folders.svg';
-import gear from './gear.svg';
-import giftCard from './gift-card.svg';
-import gift from './gift.svg';
-import gifts from './gifts.svg';
-import grid2Plus from './grid-2-plus.svg';
-import grid2 from './grid-2.svg';
-import grid from './grid.svg';
-import gripLinesVertical from './grip-lines-vertical.svg';
-import icons from './icons.svg';
-import lifeRing from './life-ring.svg';
-import listTree from './list-tree.svg';
-import list from './list.svg';
-import magnifyingGlass from './magnifying-glass.svg';
-import plus from './plus.svg';
-import receipt from './receipt.svg';
-import rectangleBarcode from './rectangle-barcode.svg';
-import shop from './shop.svg';
-import sliders from './sliders.svg';
-import star from './star.svg';
-import store from './store.svg';
-import tag from './tag.svg';
-import tags from './tags.svg';
-import triangleExclamation from './triangle-exclamation.svg';
-import truck from './truck.svg';
-import userPlus from './user-plus.svg';
-import user from './user.svg';
-import xmark from './xmark.svg';
-
-export default {
-	addressCard,
-	arrowRotateLeft,
-	arrowRotateRight,
-	badgeDollar,
-	badgePercent,
-	ban,
-	barcode,
-	basketShopping,
-	bell,
-	bookmark,
-	boxOpenFull,
-	boxesStacked,
-	calculatorSimple,
-	calculator,
-	calendarDays,
-	calendar,
-	cartPlus,
-	cartShopping,
-	cashRegister,
-	check,
-	circleCheck,
-	circle,
-	circleChevronRight,
-	circleExclamation,
-	circleInfo,
-	circleMinus,
-	circlePause,
-	circlePlus,
-	circleQuestion,
-	circleXmark,
-	clock,
-	commentsQuestion,
-	creditCard,
-	ellipsisVertical,
-	folders,
-	gear,
-	giftCard,
-	gift,
-	gifts,
-	grid2Plus,
-	grid2,
-	grid,
-	gripLinesVertical,
-	icons,
-	lifeRing,
-	listTree,
-	list,
-	magnifyingGlass,
-	plus,
-	receipt,
-	rectangleBarcode,
-	shop,
-	sliders,
-	star,
-	store,
-	tag,
-	tags,
-	userPlus,
-	user,
-	triangleExclamation,
-	truck,
-	xmark,
-};
+export { default as addressCard } from './address-card.svg';
+export { default as arrowRotateLeft } from './arrow-rotate-left.svg';
+export { default as arrowRotateRight } from './arrow-rotate-right.svg';
+export { default as badgeDollar } from './badge-dollar.svg';
+export { default as badgePercent } from './badge-percent.svg';
+export { default as ban } from './ban.svg';
+export { default as barcode } from './barcode.svg';
+export { default as basketShopping } from './basket-shopping.svg';
+export { default as bell } from './bell.svg';
+export { default as bookmark } from './bookmark.svg';
+export { default as boxOpenFull } from './box-open-full.svg';
+export { default as boxesStacked } from './boxes-stacked.svg';
+export { default as calculator } from './calculator.svg';
+export { default as calculatorSimple } from './calculator-simple.svg';
+export { default as calendar } from './calendar.svg';
+export { default as calendarDays } from './calendar-days.svg';
+export { default as cartPlus } from './cart-plus.svg';
+export { default as cartShopping } from './cart-shopping.svg';
+export { default as cashRegister } from './cash-register.svg';
+export { default as check } from './check.svg';
+export { default as circle } from './circle.svg';
+export { default as circleCheck } from './circle-check.svg';
+export { default as circleChevronRight } from './circle-chevron-right.svg';
+export { default as circleExclamation } from './circle-exclamation.svg';
+export { default as circleInfo } from './circle-info.svg';
+export { default as circleMinus } from './circle-minus.svg';
+export { default as circlePause } from './circle-pause.svg';
+export { default as circlePlus } from './circle-plus.svg';
+export { default as circleQuestion } from './circle-question.svg';
+export { default as circleXmark } from './circle-xmark.svg';
+export { default as clock } from './clock.svg';
+export { default as commentsQuestion } from './comments-question.svg';
+export { default as creditCard } from './credit-card.svg';
+export { default as ellipsisVertical } from './ellipsis-vertical.svg';
+export { default as folders } from './folders.svg';
+export { default as gear } from './gear.svg';
+export { default as gift } from './gift.svg';
+export { default as giftCard } from './gift-card.svg';
+export { default as gifts } from './gifts.svg';
+export { default as grid } from './grid.svg';
+export { default as grid2 } from './grid-2.svg';
+export { default as grid2Plus } from './grid-2-plus.svg';
+export { default as gripLinesVertical } from './grip-lines-vertical.svg';
+export { default as icons } from './icons.svg';
+export { default as lifeRing } from './life-ring.svg';
+export { default as list } from './list.svg';
+export { default as listTree } from './list-tree.svg';
+export { default as magnifyingGlass } from './magnifying-glass.svg';
+export { default as plus } from './plus.svg';
+export { default as receipt } from './receipt.svg';
+export { default as rectangleBarcode } from './rectangle-barcode.svg';
+export { default as shop } from './shop.svg';
+export { default as sliders } from './sliders.svg';
+export { default as star } from './star.svg';
+export { default as store } from './store.svg';
+export { default as tag } from './tag.svg';
+export { default as tags } from './tags.svg';
+export { default as triangleExclamation } from './triangle-exclamation.svg';
+export { default as truck } from './truck.svg';
+export { default as user } from './user.svg';
+export { default as userPlus } from './user-plus.svg';
+export { default as xmark } from './xmark.svg';
