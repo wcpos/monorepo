1d45224cbb6b193fefc10dbb30e56b3a89e0b818
kilbot <paul@kilbot.com.au>
2022-02-01 13:40:36 +0000

    update deps

--- packages/babel/index.js
+++ packages/babel/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function () {
 	return {
-		presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+		presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }], '@babel/preset-typescript'],
 		plugins: ['react-native-reanimated/plugin'],
 	};
 };
