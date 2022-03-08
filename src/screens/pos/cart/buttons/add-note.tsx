9d60fd16d84f872a480d360c2bd0ad4ec8294bd4
kilbot <paul@kilbot.com.au>
2022-03-08 13:06:24 +0000

    to JSON plugin

--- src/screens/pos/cart/buttons/add-note.tsx
+++ src/screens/pos/cart/buttons/add-note.tsx
@@ -9,14 +9,23 @@ interface AddNoteButtonProps {
 
 const AddNoteButton = ({ order }: AddNoteButtonProps) => {
 	const { ref, open, close } = useModal();
+	const [note, setNote] = React.useState('');
 
-	// order.atomicPatch({ customer_note: 'This is a note!' });
+	const handleSaveNote = React.useCallback(
+		() => order.atomicPatch({ customer_note: note }),
+		[note, order]
+	);
 
 	return (
 		<>
-			<Button title="Order Meta" background="outline" onPress={open} />
-			<Modal ref={ref} title="Edit Order">
-				<TextInput label="Order Note" />
+			<Button title="Order Note" background="outline" onPress={open} />
+			<Modal
+				ref={ref}
+				title="Order Note"
+				primaryAction={{ label: 'Save', action: handleSaveNote }}
+				secondaryActions={[{ label: 'Cancel', action: close }]}
+			>
+				<TextInput label="Order Note" value={note} onChange={setNote} />
 			</Modal>
 		</>
 	);
--- src/screens/pos/cart/buttons/save-order.tsx
+++ src/screens/pos/cart/buttons/save-order.tsx
@@ -1,51 +1,32 @@
 import * as React from 'react';
 import Button from '@wcpos/common/src/components/button';
-import useAppState from '@wcpos/common/src/hooks/use-app-state';
-import http from 'axios';
+import useRestHttpClient from '@wcpos/common/src/hooks/use-rest-http-client';
 
 interface SaveButtonProps {
 	order: import('@wcpos/common/src/database').OrderDocument;
 }
 
 const SaveButton = ({ order }: SaveButtonProps) => {
-	const { site, wpCredentials } = useAppState();
+	const http = useRestHttpClient();
 
 	const saveOrder = React.useCallback(async () => {
-		const headers = {
-			'X-WCPOS': '1',
-		};
-		if (wpCredentials.wpNonce) {
-			Object.assign(headers, { 'X-WP-Nonce': wpCredentials.wpNonce });
-		}
-		if (wpCredentials.jwt) {
-			Object.assign(headers, { Authorization: `Bearer ${wpCredentials.jwt}` });
-		}
-
-		console.log(site.getWcApiUrl());
-
 		const data = await order.toRestApiJSON();
 		let endpoint = 'orders';
 		if (order.id) {
 			endpoint += `/${order.id}`;
 		}
 
-		const result = await http(
-			// @ts-ignore
-			endpoint,
-			{
-				method: 'post',
-				baseURL: site.getWcApiUrl(),
-				headers,
-				data,
-			}
-		).catch(({ response }) => {
+		const result = await http(endpoint, {
+			method: 'post',
+			data,
+		}).catch(({ response }) => {
 			debugger;
 		});
 
-		if (result.status === 201) {
+		if (result.status === 201 || result.status === 200) {
 			order.atomicPatch(result.data);
 		}
-	}, [order, site, wpCredentials.jwt, wpCredentials.wpNonce]);
+	}, [http, order]);
 
 	return <Button title="Save" background="outline" onPress={saveOrder} />;
 };
