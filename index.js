c8fb60311d3b66ac344b7d832b00073367276ff0
kilbot <paul@kilbot.com.au>
2022-08-04 18:53:54 +0100

    add sentry

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import { registerRootComponent } from 'expo';
+import * as Sentry from 'sentry-expo';
 
 import App from '@wcpos/core';
+
+Sentry.init({
+	dsn: 'https://39233e9d1e5046cbb67dae52f807de5f@o159038.ingest.sentry.io/1220733',
+	enableInExpoDevelopment: true,
+	debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
