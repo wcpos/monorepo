7b24bd53363e4fc21958deb561cd4febd00431a3
kilbot <paul@kilbot.com.au>
2022-08-05 19:01:21 +0100

    add Sentry token

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import App from '@wcpos/core';
 
 Sentry.init({
 	dsn: 'https://39233e9d1e5046cbb67dae52f807de5f@o159038.ingest.sentry.io/1220733',
-	enableInExpoDevelopment: true,
-	debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+	enableInExpoDevelopment: false,
+	debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
