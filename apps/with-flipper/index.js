181922ea6fdbe9db061ff1b83ea86512f4eaf7b2
kilbot <paul@kilbot.com.au>
2022-08-19 18:49:41 +0100

    update packages

--- apps/with-flipper/index.js
+++ apps/with-flipper/index.js
@@ -1,6 +1,14 @@
-import { registerRootComponent } from "expo";
+import { registerRootComponent } from 'expo';
+import { connectToDevTools } from 'react-devtools-core';
 
-import App from "./App";
+import App from '@wcpos/core';
+
+if (__DEV__) {
+	connectToDevTools({
+		host: 'localhost',
+		port: 8097,
+	});
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
