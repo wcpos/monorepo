c7f0c52d7ec5d42e58c49f8e564ea65eeb2c7ea8
kilbot <paul@kilbot.com.au>
2022-10-04 17:55:04 +0100

    update web to indexeddb + memory-synced storage

--- src/adapter/index.electron.ts
+++ src/adapter/index.electron.ts
@@ -2,6 +2,7 @@ import { clone } from 'rxdb';
 import { getRxStorageSQLite } from './plugins/sqlite';
 import { mangoQuerySelectorToSQL } from './mangoQuerySelectorToSQL';
 import { mangoQuerySortToSQL } from './plugins/sqlite/sqlite-statics';
+// import { getMemorySyncedRxStorage } from './plugins/memory-synced';
 
 import type { SQLiteQueryWithParams } from './plugins/sqlite';
 
@@ -47,20 +48,46 @@ const sqliteBasics = {
 	journalMode: '',
 };
 
+const parentStorage = getRxStorageSQLite({ sqliteBasics });
+
 /**
  *
  */
 const config = {
-	storage: getRxStorageSQLite({
-		/**
-		 * Different runtimes have different interfaces to SQLite.
-		 * For example in node.js we have a callback API,
-		 * while in capacitor sqlite we have Promises.
-		 * So we need a helper object that is capable of doing the basic
-		 * sqlite operations.
-		 */
-		sqliteBasics,
-	}),
+	storage: parentStorage,
+	// storage: getMemorySyncedRxStorage({
+	// 	storage: parentStorage,
+
+	// 	/**
+	// 	 * Defines how many document
+	// 	 * get replicated in a single batch.
+	// 	 * [default=50]
+	// 	 *
+	// 	 * (optional)
+	// 	 */
+	// 	batchSize: 50,
+
+	// 	/**
+	// 	 * By default, the parent storage will be created without indexes for a faster page load.
+	// 	 * Indexes are not needed because the queries will anyway run on the memory storage.
+	// 	 * You can disable this behavior by setting keepIndexesOnParent to true.
+	// 	 *
+	// 	 * (optional)
+	// 	 */
+	// 	keepIndexesOnParent: true,
+
+	// 	/**
+	// 	 * After a write, await until the return value of this method resolves
+	// 	 * before replicating with the master storage.
+	// 	 *
+	// 	 * By returning requestIdlePromise() we can ensure that the CPU is idle
+	// 	 * and no other, more important operation is running. By doing so we can be sure
+	// 	 * that the replication does not slow down any rendering of the browser process.
+	// 	 *
+	// 	 * (optional)
+	// 	 */
+	// 	waitBeforePersist: () => requestIdlePromise(),
+	// }),
 	multiInstance: false,
 	ignoreDuplicate: true,
 };
--- src/adapter/index.web.ts
+++ src/adapter/index.web.ts
@@ -1,15 +1,42 @@
+import { requestIdlePromise } from 'rxdb';
 import { getRxStorageIndexedDB } from './plugins/indexeddb';
+import { getMemorySyncedRxStorage } from './plugins/memory-synced';
+
+const parentStorage = getRxStorageIndexedDB();
 
 const config = {
-	storage: getRxStorageIndexedDB({
+	storage: getMemorySyncedRxStorage({
+		storage: parentStorage,
+
 		/**
-		 * For better performance, queries run with a batched cursor.
-		 * You can change the batchSize to optimize the query time
-		 * for specific queries.
-		 * You should only change this value when you are also doing performance measurements.
+		 * Defines how many document
+		 * get replicated in a single batch.
 		 * [default=50]
+		 *
+		 * (optional)
 		 */
 		batchSize: 50,
+
+		/**
+		 * By default, the parent storage will be created without indexes for a faster page load.
+		 * Indexes are not needed because the queries will anyway run on the memory storage.
+		 * You can disable this behavior by setting keepIndexesOnParent to true.
+		 *
+		 * (optional)
+		 */
+		keepIndexesOnParent: true,
+
+		/**
+		 * After a write, await until the return value of this method resolves
+		 * before replicating with the master storage.
+		 *
+		 * By returning requestIdlePromise() we can ensure that the CPU is idle
+		 * and no other, more important operation is running. By doing so we can be sure
+		 * that the replication does not slow down any rendering of the browser process.
+		 *
+		 * (optional)
+		 */
+		waitBeforePersist: () => requestIdlePromise(),
 	}),
 };
 
