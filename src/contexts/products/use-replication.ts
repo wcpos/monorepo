bfb7bc8082827e1045bda0be1d058a648aba14a9
kilbot <paul@kilbot.com.au>
2022-10-20 11:53:27 +0100

    update audit and replicate functions

--- src/contexts/products/use-replication.ts
+++ src/contexts/products/use-replication.ts
@@ -2,42 +2,110 @@ import * as React from 'react';
 import { replicateRxCollection } from 'rxdb/plugins/replication';
 import useRestHttpClient from '@wcpos/hooks/src/use-rest-http-client';
 import useAuth from '@wcpos/hooks/src/use-auth';
+import isEmpty from 'lodash/isEmpty';
+
+/**
+ * Hack, I want the replication to wait before looping to allow counts to be updated
+ */
+function wait(milliseconds: number) {
+	return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
 
 export const useReplication = ({ collection }) => {
 	const http = useRestHttpClient();
 	const { site } = useAuth();
 
 	const replicationStatePromise = React.useMemo(() => {
+		/**
+		 *
+		 */
+		const audit = async () => {
+			const response = await http
+				.get(collection.name, {
+					params: { fields: ['id', 'name'], posts_per_page: -1 },
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+
+			/**
+			 * What to do when server is unreachable?
+			 */
+			if (!response?.data) {
+				throw Error('No response from server');
+			}
+
+			const documents = await collection.auditRestApiIds(response?.data);
+
+			//
+			return {
+				documents,
+				checkpoint: {
+					audit: false,
+				},
+			};
+		};
+
+		/**
+		 *
+		 */
+		const replicate = async (lastCheckpoint, batchSize) => {
+			/**
+			 * This is the data replication
+			 * we need to delay for a little while to allow the collection count to be updated
+			 */
+			await wait(1000);
+			const pullRemoteIds = collection.pullRemoteIds$.getValue();
+			const syncedDocs = collection.syncedIds$.getValue();
+
+			if (pullRemoteIds.length === 0) {
+				return {
+					documents: [],
+					checkpoint: { audit: false },
+				};
+			}
+
+			const params = {
+				order: 'asc',
+				orderby: 'title',
+			};
+
+			// choose the smallest array, max of 1000
+			if (syncedDocs.length > pullRemoteIds.length) {
+				params.include = pullRemoteIds.slice(0, 1000).join(',');
+			} else {
+				params.exclude = syncedDocs.slice(0, 1000).join(',');
+			}
+
+			const response = await http.get(collection.name, { params }).catch((error) => {
+				console.log(error);
+			});
+
+			/**
+			 * What to do when server is unreachable?
+			 */
+			if (!response?.data) {
+				throw Error('No response from server');
+			}
+
+			return {
+				documents: response?.data || [],
+				checkpoint: { audit: false },
+			};
+		};
+
+		/**
+		 *
+		 */
 		return replicateRxCollection({
 			collection,
 			replicationIdentifier: `wc-rest-replication-to-${site.wc_api_url}/${collection.name}`,
-			retryTime: 1000000000,
+			// retryTime: 1000000000,
 			pull: {
 				async handler(lastCheckpoint, batchSize) {
-					const response = await http
-						.get(collection.name, {
-							params: { fields: ['id', 'name'], posts_per_page: -1 },
-						})
-						.catch((error) => {
-							console.log(error);
-						});
-
-					/**
-					 * What to do when server is unreachable?
-					 */
-					if (!response?.data) {
-						throw Error('No response from server');
-					}
-
-					const documents = await collection.auditRestApiIds(response?.data);
-
-					//
-					return {
-						documents,
-						checkpoint: { id: '123', updatedAt: 12342878678 },
-					};
+					return isEmpty(lastCheckpoint) ? audit() : replicate(lastCheckpoint, batchSize);
 				},
-				batchSize: 100,
+				batchSize: 10,
 				modifier: (doc) => collection.parseRestResponse(doc),
 				// stream$: timedObservable(1000),
 			},
