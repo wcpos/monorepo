cf6d3dc4ccb7262975a7e45eca125ca92cf02560
Paul Kilmurray <paul@kilbot.com>
2023-03-22 17:18:31 +0000

    improve logout hack

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -53,7 +53,9 @@ const MainNavigator = () => {
 		 * FIXME - this is a hack to avoid the app crashing when site is null
 		 * We need to find a better way to handle this
 		 */
-		throw new Promise(() => {});
+		throw new Promise((resolve, reject) => {
+			resolve(true);
+		});
 	}
 	const wpAPIURL = useObservableState(site.wp_api_url$, site.wp_api_url);
 	const dimensions = useWindowDimensions();
