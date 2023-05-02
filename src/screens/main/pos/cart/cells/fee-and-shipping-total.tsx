2cfb23fffe83ba2d0cada6bdb2f2f2c81a718258
Paul Kilmurray <paul@kilbot.com>
2023-05-02 07:30:27 +0100

    fix fee and shiping total

--- src/screens/main/pos/cart/cells/fee-and-shipping-total.tsx
+++ src/screens/main/pos/cart/cells/fee-and-shipping-total.tsx
@@ -4,10 +4,8 @@ import find from 'lodash/find';
 import { useObservableState } from 'observable-hooks';
 
 import Box from '@wcpos/components/src/box';
-import Checkbox from '@wcpos/components/src/checkbox';
 import Text from '@wcpos/components/src/text';
 
-import { t } from '../../../../../lib/translations';
 import NumberInput from '../../../components/number-input';
 import useCurrencyFormat from '../../../hooks/use-currency-format';
 
@@ -18,14 +16,11 @@ interface Props {
 /**
  * Changing the total actually updates the price, because the WC REST API makes no sense
  */
-export const Total = ({ item, column }: Props) => {
+export const FeeAndShippingTotal = ({ item, column }: Props) => {
 	const total = useObservableState(item.total$, item.total);
-	const subtotal = useObservableState(item.subtotal$, item.subtotal);
 	const total_tax = useObservableState(item.total_tax$, item.total_tax);
 	const { format } = useCurrencyFormat();
 	const { display } = column;
-	// const [editable, setEditable] = React.useState(parseFloat(total) !== parseFloat(subtotal));
-	const editable = parseFloat(total) !== parseFloat(subtotal);
 
 	/**
 	 *
@@ -55,42 +50,14 @@ export const Total = ({ item, column }: Props) => {
 	/**
 	 *
 	 */
-	// const handleOnSaleChange = React.useCallback(
-	// 	(onSale) => {
-	// 		if (onSale) {
-	// 			item.incrementalPatch({
-	// 				total: subtotal,
-	// 			});
-	// 		}
-	// 		setEditable(onSale);
-	// 	},
-	// 	[item, subtotal]
-	// );
-
-	/**
-	 *
-	 */
 	return (
 		<Box space="xSmall" align="end">
-			{editable ? (
-				<NumberInput value={total} onChange={handleUpdate} showDecimals />
-			) : (
-				<Text>{format(subtotal || 0)}</Text>
-			)}
-
+			<NumberInput value={total} onChange={handleUpdate} showDecimals />
 			{show('tax') && (
 				<Text type="textMuted" size="small">
 					{`excl. ${format(total_tax) || 0} tax`}
 				</Text>
 			)}
-
-			{/* <Checkbox
-				value={editable}
-				label={t('On sale', { _tags: 'core' })}
-				size="xSmall"
-				type="secondary"
-				onChange={handleOnSaleChange}
-			/> */}
 		</Box>
 	);
 };
--- src/screens/main/pos/cart/cells/index.ts
+++ src/screens/main/pos/cart/cells/index.ts
@@ -1,12 +1,13 @@
 import { Actions } from './actions';
 import { FeeAndShippingPrice } from './fee-and-shipping-price';
+import { FeeAndShippingTotal } from './fee-and-shipping-total';
 import { FeeName } from './fee-name';
 import { Price } from './price';
+import { PriceTotal } from './price-total';
 import { ProductName } from './product-name';
 import { Quantity } from './quantity';
 import { ShippingTitle } from './shipping-title';
 import { Subtotal } from './subtotal';
-import { Total } from './total';
 
 const line_items = {
 	actions: Actions,
@@ -14,21 +15,21 @@ const line_items = {
 	price: Price,
 	quantity: Quantity,
 	subtotal: Subtotal,
-	total: Total,
+	total: PriceTotal,
 };
 
 const fee_lines = {
 	actions: Actions,
 	name: FeeName,
 	price: FeeAndShippingPrice,
-	total: Total,
+	total: FeeAndShippingTotal,
 };
 
 const shipping_lines = {
 	actions: Actions,
 	name: ShippingTitle,
 	price: FeeAndShippingPrice,
-	total: Total,
+	total: FeeAndShippingTotal,
 };
 
 export { line_items, fee_lines, shipping_lines };
