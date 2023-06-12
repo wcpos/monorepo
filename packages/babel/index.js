8a9a1064a27b473c7cbb4b0ed296e64fe79b5367
Paul Kilmurray <paul@kilbot.com>
2023-06-12 14:23:39 +0100

    update packages

--- packages/babel/index.js
+++ packages/babel/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = function () {
 	return {
 		presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }], '@babel/preset-typescript'],
-		plugins: ['react-native-reanimated/plugin'],
+		plugins: ['react-native-reanimated/plugin', '@babel/plugin-proposal-export-namespace-from'],
 	};
 };
