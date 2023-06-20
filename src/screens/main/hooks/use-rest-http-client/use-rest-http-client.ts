53687f0ef90d703be97a276740d43ce60244015d
Paul Kilmurray <paul@kilbot.com>
2023-06-20 23:57:51 +0100

    refresh the cookie nonce

--- src/screens/main/hooks/use-rest-http-client/use-rest-http-client.ts
+++ src/screens/main/hooks/use-rest-http-client/use-rest-http-client.ts
@@ -27,16 +27,46 @@ export const useRestHttpClient = () => {
 
 		if (wp_nonce) {
 			set(config, ['headers', 'X-WP-Nonce'], wp_nonce);
-		}
-
-		if (jwt) {
+		} else if (jwt) {
 			set(config, ['headers', 'Authorization'], `Bearer ${jwt}`);
 		}
 
 		const instance = httpClient.create(config);
 
+		/**
+		 * HACK - this is going to add a new interceptor every time this hook is called
+		 * _interceptorAdded is a dodgy way to prevent this from happening
+		 */
+		if (
+			!instance.axios._interceptorAdded &&
+			instance.axios &&
+			instance.axios.interceptors &&
+			instance.axios.interceptors.response
+		) {
+			instance.axios._interceptorAdded = true;
+			instance.axios.interceptors.response.use(
+				(response) => {
+					// Any status code that lie within the range of 2xx cause this function to trigger
+					// compare the respone headers to the wp_nonce and update if needed
+					const new_wp_nonce = response.headers['x-wp-nonce'];
+					if (new_wp_nonce && new_wp_nonce !== wp_nonce) {
+						wpCredentials.incrementalPatch({ wp_nonce: new_wp_nonce });
+					}
+
+					return response;
+				},
+				(error) => {
+					// Any status codes that falls outside the range of 2xx cause this function to trigger
+					// Do something with response error
+					if (error.response && error.response.status === 401) {
+					}
+					return Promise.reject(error);
+				}
+			);
+		}
+
 		return instance;
-	}, [baseURL, httpClient, jwt, wp_nonce]);
+	}, [baseURL, httpClient, jwt, wpCredentials, wp_nonce]);
 
 	/**
 	 *
