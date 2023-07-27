6f2f40d0ff30a0448c79cc425afa44c4ef47e126
Paul Kilmurray <paul@kilbot.com>
2023-07-27 13:28:09 +0100

    v1.3

--- metro.config.js
+++ metro.config.js
@@ -1,40 +1,40 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-
-// Add support for svg files: https://github.com/react-native-svg/react-native-svg#use-with-svg-files
+const fs = require('fs');
+const path = require('path');
 
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-const fs = require('fs');
-
-function getWorkspaceRoot() {
-	const packageFile = path.join(__dirname, '../../package.json');
-	const packageContent = fs.existsSync(packageFile) ? fs.readFileSync(packageFile, 'utf-8') : null;
-	if (packageContent && JSON.parse(packageContent).name === '@wcpos/monorepo') {
-		console.log('Found workspace root');
-		return path.resolve(__dirname, '../..');
-	}
-	return null;
-}
-
-const projectRoot = __dirname;
-const workspaceRoot = getWorkspaceRoot();
 
 module.exports = (() => {
 	const config = getDefaultConfig(__dirname);
 
-	const {
-		resolver: { sourceExts, assetExts },
-	} = config;
+	// I need to add mjs
+	console.log(config.resolver.sourceExts);
+	config.resolver.sourceExts.push('mjs');
 
-	if (workspaceRoot) {
-		config.watchFolders = [workspaceRoot];
-	}
-
-	config.resolver.disableHierarchicalLookup = true;
-	config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
-
-	if (workspaceRoot) {
-		config.resolver.nodeModulesPaths.push(path.resolve(workspaceRoot, 'node_modules'));
+	//
+	if (process.env.ELECTRON === 'true') {
+		config.resolver.sourceExts = [
+			'electron.ts',
+			'electron.tsx',
+			'electron.js',
+			'electron.jsx',
+			'electron.json',
+			'electron.cjs',
+			'electron.mjs',
+			'web.ts',
+			'web.tsx',
+			'web.js',
+			'web.jsx',
+			'web.json',
+			'web.cjs',
+			'web.mjs',
+			'ts',
+			'tsx',
+			'js',
+			'jsx',
+			'json',
+			'cjs',
+			'mjs',
+		];
 	}
 
 	return config;
