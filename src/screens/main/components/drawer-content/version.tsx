c2ce84c7103af35eca41ebf37d262befce0441e9
Paul Kilmurray <paul@kilbot.com>
2023-07-29 09:27:00 +0100

    bump version

--- src/screens/main/components/drawer-content/version.tsx
+++ src/screens/main/components/drawer-content/version.tsx
@@ -11,7 +11,7 @@ const Version = ({ largeScreen }) => {
 	return (
 		<Box paddingY="xxSmall" paddingX={largeScreen ? 'none' : 'large'}>
 			<Text type="darkestGrey" size="xSmall" align={largeScreen ? 'center' : 'left'}>
-				{largeScreen ? 'v 1.3.9' : 'Version 1.3.9'}
+				{largeScreen ? 'v 1.3.10' : 'Version 1.3.10'}
 			</Text>
 		</Box>
 	);
