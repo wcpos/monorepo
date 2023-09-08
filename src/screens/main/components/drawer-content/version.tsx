1f488583e419bba3ef3c14281d8853e83e265f26
Paul Kilmurray <paul@kilbot.com>
2023-09-08 20:04:32 +0100

    deafult customer hack

--- src/screens/main/components/drawer-content/version.tsx
+++ src/screens/main/components/drawer-content/version.tsx
@@ -11,7 +11,7 @@ const Version = ({ largeScreen }) => {
 	return (
 		<Box paddingY="xxSmall" paddingX={largeScreen ? 'none' : 'large'}>
 			<Text type="darkestGrey" size="xSmall" align={largeScreen ? 'center' : 'left'}>
-				{largeScreen ? 'v 1.3.15' : 'Version 1.3.15'}
+				{largeScreen ? 'v 1.3.16' : 'Version 1.3.16'}
 			</Text>
 		</Box>
 	);
--- src/screens/main/hooks/use-default-customer.ts
+++ src/screens/main/hooks/use-default-customer.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { ObservableResource } from 'observable-hooks';
-import { of, lastValueFrom } from 'rxjs';
+import { of, lastValueFrom, startWith } from 'rxjs';
 
 import { useDefaultCustomerID } from './use-default-customer-id';
 import { useGetDocumentByRemoteId } from '../hooks/use-get-document-by-remote-id';
@@ -15,7 +15,8 @@ export const useDefaultCustomer = () => {
 		remoteID: defaultCustomerID,
 		fallback: guestCustomer,
 	});
-	const defaultCustomer$ = defaultCustomerID === 0 ? of(guestCustomer) : document$;
+	const _hack$ = document$.pipe(startWith(guestCustomer));
+	const defaultCustomer$ = defaultCustomerID === 0 ? of(guestCustomer) : _hack$;
 	const defaultCustomerPromise = lastValueFrom(defaultCustomer$);
 
 	const defaultCustomerResource = React.useMemo(
