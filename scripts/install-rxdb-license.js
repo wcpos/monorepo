6c550b31bd8dd28e89682531b626f62745df4eea
Paul Kilmurray <paul@kilbot.com>
2024-06-03 15:59:35 +0100

    change script to commonjs

--- scripts/install-rxdb-license.js
+++ scripts/install-rxdb-license.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as dotenv from 'dotenv';
-import { readJson, writeJson } from 'fs-extra';
+const dotenv = require('dotenv');
+const { readJson, writeJson } = require('fs-extra');
+
+console.log('Starting preinstall script...');
 
 async function addLicenseToPackageJson() {
 	dotenv.config();
@@ -13,6 +15,7 @@ async function addLicenseToPackageJson() {
 
 	const packageJsonPath = './package.json'; // Adjust path as necessary
 	try {
+		console.log('Reading package.json...');
 		const packageJson = await readJson(packageJsonPath);
 
 		// Ensuring accessTokens object exists
@@ -23,6 +26,7 @@ async function addLicenseToPackageJson() {
 		// Adding the license key to rxdb-premium in accessTokens
 		packageJson.accessTokens['rxdb-premium'] = licenseKey;
 
+		console.log('Writing updated package.json...');
 		await writeJson(packageJsonPath, packageJson, { spaces: 2 });
 		console.log('License key added to package.json under accessTokens');
 	} catch (error) {
