e6f44fb0a726da0d5d110b5a375ea26420da91ad
Paul Kilmurray <paul@kilbot.com>
2024-07-24 20:08:35 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,9 +44,33 @@ module.exports = {
 					DEFAULT: 'hsl(var(--secondary))',
 					foreground: 'hsl(var(--secondary-foreground))',
 				},
+				tertiary: {
+					DEFAULT: 'hsl(var(--tertiary))',
+					foreground: 'hsl(var(--tertiary-foreground))',
+				},
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				success: {
+					DEFAULT: 'hsl(var(--success))',
+					foreground: 'hsl(var(--success-foreground))',
+				},
+				warning: {
+					DEFAULT: 'hsl(var(--warning))',
+					foreground: 'hsl(var(--warning-foreground))',
+				},
+				attention: {
+					DEFAULT: 'hsl(var(--attention))',
+					foreground: 'hsl(var(--attention-foreground))',
+				},
+				info: {
+					DEFAULT: 'hsl(var(--info))',
+					foreground: 'hsl(var(--info-foreground))',
+				},
+				error: {
+					DEFAULT: 'hsl(var(--error))',
+					foreground: 'hsl(var(--error-foreground))',
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
