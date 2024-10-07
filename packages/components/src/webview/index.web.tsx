aa07566235f580747a0abaed0af6498759e6db33
Paul Kilmurray <paul@kilbot.com>
2024-10-08 01:06:06 +0200

    update webview component

--- packages/components/src/webview/index.web.tsx
+++ packages/components/src/webview/index.web.tsx
@@ -1,103 +1,87 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import { cn } from '../lib/utils';
+import { Loader } from '../loader';
 
-import type { WebViewSharedProps } from 'react-native-webview/lib/WebViewTypes';
+import type {
+	WebViewSharedProps,
+	WebViewNavigationEvent,
+} from 'react-native-webview/lib/WebViewTypes';
 
-export interface WebViewProps extends Omit<WebViewSharedProps, 'style'> {
-	style?: React.CSSProperties;
-	/**
-	 * Source URI helper
-	 */
+export interface WebViewProps extends WebViewSharedProps {
 	src?: string;
-	/**
-	 * A string that represents the content to display in the frame.
-	 */
 	srcDoc?: string;
 }
 
 /**
  *
  */
-const WebViewBase = (
-	{
-		style,
-		src,
-		source,
-		onMessage,
-		onLoad,
-		injectedJavaScript, // not working at the moment
-		onNavigationStateChange,
-		onError,
-		srcDoc,
-		className,
-	}: WebViewProps,
-	ref
-) => {
-	const iframeRef = React.useRef<HTMLIFrameElement>(null);
-	React.useImperativeHandle(ref, () => iframeRef.current);
+const _WebView = React.forwardRef<HTMLIFrameElement, WebViewProps>(
+	({ src, source, onMessage, onLoad, srcDoc, className, ...props }, ref) => {
+		const [loading, setLoading] = React.useState(true);
 
-	// const onIframeLoad = function () {
-	//   try {
-	//     const location = iframeRef.current?.contentWindow?.location;
-	//     console.log("onIframeLoad location", location);
+		/**
+		 * Attach message listener
+		 */
+		React.useEffect(() => {
+			const onIframeMessage = (event: MessageEvent<any>) => {
+				const { origin, data } = event;
 
-	//     onNavigationStateChange?.({
-	//       url: location?.href || "",
-	//       navigationType: "other",
-	//       loading: false,
-	//       title: "",
-	//       canGoBack: false,
-	//       canGoForward: false,
-	//       lockIdentifier: 0,
-	//     });
-	//   } catch (e) {
-	//     throw new Error(e);
-	//   }
-	// };
+				const message = {
+					data,
+					url: origin,
+					loading: false,
+					title: '',
+					canGoBack: false,
+					canGoForward: false,
+					lockIdentifier: 0,
+				};
 
-	/**
-	 * attach message listener
-	 */
-	React.useEffect(() => {
-		const onIframeMessage = (event: MessageEvent<any>) => {
-			const { origin, data } = event;
-
-			const message = {
-				data,
-				url: origin,
-				loading: false,
-				title: '',
-				canGoBack: false,
-				canGoForward: false,
-				lockIdentifier: 0,
+				onMessage?.(message as any);
 			};
 
-			onMessage?.(message as any);
-		};
+			window.addEventListener('message', onIframeMessage, true);
 
-		window.addEventListener('message', onIframeMessage, true);
+			return () => {
+				window.removeEventListener('message', onIframeMessage, true);
+			};
+		}, [onMessage]);
 
-		return () => {
-			window.removeEventListener('message', onIframeMessage, true);
-		};
-	}, [onMessage]);
+		/**
+		 * Handle loaded
+		 */
+		const handleLoaded = React.useCallback(
+			(e: WebViewNavigationEvent) => {
+				setLoading(false);
+				onLoad?.(e);
+			},
+			[onLoad]
+		);
 
-	/**
-	 * attach message listener
-	 */
+		/**
+		 *
+		 */
+		return (
+			<View className={cn('relative', className)}>
+				<iframe
+					ref={ref}
+					src={source?.uri || src}
+					srcDoc={srcDoc}
+					onLoad={handleLoaded}
+					frameBorder="0"
+					sandbox="allow-scripts allow-forms allow-popups allow-same-origin allow-modals"
+					className="w-full h-full"
+					{...props}
+				/>
+				{loading && (
+					<View className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75">
+						<Loader />
+					</View>
+				)}
+			</View>
+		);
+	}
+);
 
-	return (
-		<iframe
-			ref={iframeRef}
-			src={source?.uri || src}
-			srcDoc={srcDoc}
-			onLoad={onLoad}
-			frameBorder="0"
-			sandbox="allow-scripts allow-forms allow-popups allow-same-origin"
-			className={cn('w-full h-full', className)}
-		/>
-	);
-};
-
-export const WebView = React.forwardRef<HTMLIFrameElement, WebViewProps>(WebViewBase);
+export const WebView = React.memo(_WebView);
