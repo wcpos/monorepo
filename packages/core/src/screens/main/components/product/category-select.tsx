22daf31ab81424ea914ae16eab914464e2745362
Paul Kilmurray <paul@kilbot.com>
2025-04-13 20:47:28 +0200

    refactor category select

---
+++ packages/core/src/screens/main/components/product/category-select.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+
+import { useObservableSuspense } from 'observable-hooks';
+
+import {
+	Combobox,
+	ComboboxTrigger,
+	ComboboxContent,
+	ComboboxValue,
+	ComboboxSearch,
+	ComboboxInput,
+	ComboboxEmpty,
+	ComboboxList,
+} from '@wcpos/components/combobox';
+import { Suspense } from '@wcpos/components/suspense';
+import { useT } from '@wcpos/core/contexts/translations';
+import { useQuery } from '@wcpos/query';
+
+/**
+ *
+ */
+const CategoryList = ({ query }) => {
+	const result = useObservableSuspense(query.resource);
+	const t = useT();
+
+	const data = result.hits.map(({ id, document }) => ({
+		value: String(document.id),
+		label: document.name,
+	}));
+
+	return (
+		<ComboboxList
+			data={data}
+			onEndReached={() => {
+				if (query?.infiniteScroll) {
+					query.loadMore();
+				}
+			}}
+			ListEmptyComponent={
+				<ComboboxEmpty>{t('No category found', { _tags: 'core' })}</ComboboxEmpty>
+			}
+		/>
+	);
+};
+
+/**
+ *
+ */
+export const CategorySearch = () => {
+	const t = useT();
+	const [search, setSearch] = React.useState('');
+	console.log('category search');
+
+	/**
+	 *
+	 */
+	const query = useQuery({
+		queryKeys: ['products/categories'],
+		collectionName: 'products/categories',
+		initialParams: {
+			sort: [{ name: 'asc' }],
+		},
+		greedy: true,
+		infiniteScroll: true,
+	});
+
+	/**
+	 *
+	 */
+	const onSearch = React.useCallback(
+		(value: string) => {
+			setSearch(value);
+			query.debouncedSearch(value);
+		},
+		[query]
+	);
+
+	/**
+	 * Clear the search when unmounting
+	 */
+	React.useEffect(() => {
+		return () => query.search('');
+	}, [query]);
+
+	/**
+	 *
+	 */
+	return (
+		<>
+			<ComboboxInput
+				placeholder={t('Search Categories', { _tags: 'core' })}
+				value={search}
+				onChangeText={onSearch}
+			/>
+			<Suspense>
+				<CategoryList query={query} />
+			</Suspense>
+		</>
+	);
+};
+
+/**
+ *
+ */
+export const CategorySelect = ({ onValueChange }) => {
+	const t = useT();
+
+	/**
+	 *
+	 */
+	return (
+		<Combobox onValueChange={onValueChange}>
+			<ComboboxTrigger>
+				<ComboboxValue placeholder={t('Select Category', { _tags: 'core' })} />
+			</ComboboxTrigger>
+			<ComboboxContent className="min-w-64">
+				<CategorySearch />
+			</ComboboxContent>
+		</Combobox>
+	);
+};
--- packages/core/src/screens/main/components/product/category-select/index.ts
+++
@@ -1,3 +0,0 @@
-export { CategoryList } from './list';
-export { CategorySearch } from './search';
-export { CategorySelect } from './select';
