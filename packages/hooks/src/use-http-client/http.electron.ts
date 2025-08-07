68baabb64c8333554cbfaccfae5665f63f2fd9c1
Paul Kilmurray <paul@kilbot.com>
2025-08-07 12:49:50 +0200

    update deps

--- packages/hooks/src/use-http-client/http.electron.ts
+++ packages/hooks/src/use-http-client/http.electron.ts
@@ -3,25 +3,41 @@
  */
 type AxiosRequestConfig = import('axios').AxiosRequestConfig;
 
+// Extend the Window interface to include ipcRenderer
+declare global {
+	interface Window {
+		ipcRenderer: {
+			invoke: (channel: string, data?: any) => Promise<any>;
+		};
+	}
+}
+
 /**
  *
  */
 const axiosOnElectronMain = {
 	request: (config: AxiosRequestConfig = {}) => {
-		return new Promise((resolve, reject) => {
-			window.ipcRenderer
-				.invoke('axios', {
-					type: 'request',
-					config,
-				})
-				.then((result) => {
-					if (result.code) {
-						reject(result);
-					} else {
-						resolve(result);
-					}
-				});
-		});
+		return window.ipcRenderer
+			.invoke('axios', {
+				type: 'request',
+				config,
+			})
+			.then((result: any) => {
+				if (result.success === false) {
+					// Create an Error object that behaves like an AxiosError
+					const error = new Error(result.message) as any;
+					error.name = result.name;
+					error.code = result.code;
+					error.config = result.config;
+					error.request = result.request;
+					error.response = result.response;
+					error.isAxiosError = result.isAxiosError;
+					throw error;
+				}
+				// Remove the success flag from successful responses
+				const { success, ...response } = result;
+				return response;
+			});
 	},
 };
 
