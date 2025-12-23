451830e529d5a6fffef9aebeca08ca093cf3c417
Paul Kilmurray <paul@kilbot.com>
2025-12-23 15:32:09 +0100

    v1.8.3

--- packages/core/src/screens/main/components/drawer-content/version.tsx
+++ packages/core/src/screens/main/components/drawer-content/version.tsx
@@ -17,7 +17,7 @@ const Version = () => {
 				screenSize === 'lg' && 'px-0 text-center'
 			)}
 		>
-			{screenSize === 'lg' ? `v 1.8.2` : `Version 1.8.2`}
+			{screenSize === 'lg' ? `v 1.8.3` : `Version 1.8.3`}
 		</Text>
 	);
 };
