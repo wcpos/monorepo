814203905c10a7c05eaa22c622e70a0045cbd7fd
Paul Kilmurray <paul@kilbot.com>
2026-03-23 20:54:33 +0100

    fix: handle possibly undefined couponData.code in TS strict mode

--- packages/core/src/screens/main/pos/hooks/use-add-coupon.ts
+++ packages/core/src/screens/main/pos/hooks/use-add-coupon.ts
@@ -158,7 +158,7 @@ export const useAddCoupon = () => {
 				}
 
 				const appliedCouponLine = result.couponLines.find(
-					(cl: any) => cl.code?.toLowerCase() === couponData.code.toLowerCase()
+					(cl: any) => cl.code?.toLowerCase() === couponData.code?.toLowerCase()
 				);
 				orderLogger.info(t('pos_cart.coupon_applied', { defaultValue: 'Coupon applied' }), {
 					context: {
