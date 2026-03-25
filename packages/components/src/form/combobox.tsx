20ecc0754f8da46a75e872b5874eefc741034c1c
Paul Kilmurray <paul@kilbot.com>
2026-03-25 14:02:47 +0100

    feat(form-combobox): support multi-select with Option[] passthrough

--- packages/components/src/form/combobox.tsx
+++ packages/components/src/form/combobox.tsx
@@ -4,24 +4,48 @@ import { FormDescription, FormItem, FormLabel, FormMessage } from './common';
 import { useFormField } from './context';
 import { Combobox } from '../combobox';
 
+import type { Option } from '../combobox';
 import type { FormItemProps } from './common';
 
 /**
- * NOTE: combobox is a bit different from the other form components
- * - the value will come in as a string and go out as a string, but
- * - the combobox component expects an object with value and label
+ * Single-select: value is a string, converted to/from Option internally.
+ * Multi-select: value is Option[], passed through directly.
  */
+type FormComboboxProps = (
+	| (FormItemProps<string> & { multiple?: false })
+	| (FormItemProps<Option[]> & { multiple: true })
+) &
+	Omit<Partial<React.ComponentProps<typeof Combobox>>, 'value' | 'onValueChange' | 'multiple'>;
+
 export function FormCombobox({
 	label,
 	description,
 	value,
 	onChange,
+	multiple,
 	customComponent: Component = Combobox,
 	...props
-}: FormItemProps<string> &
-	Omit<Partial<React.ComponentProps<typeof Combobox>>, 'value' | 'onValueChange'>) {
+}: FormComboboxProps) {
 	const [open, setOpen] = React.useState(false);
 	const { error, formItemNativeID, formDescriptionNativeID, formMessageNativeID } = useFormField();
+
+	const comboboxValue = React.useMemo(() => {
+		if (multiple) {
+			return (value as Option[] | undefined) ?? [];
+		}
+		return typeof value === 'string' ? { value, label: value } : value;
+	}, [multiple, value]);
+
+	const handleValueChange = React.useCallback(
+		(val: any) => {
+			if (multiple) {
+				onChange?.(val ?? []);
+			} else {
+				onChange?.(val?.value);
+			}
+		},
+		[multiple, onChange]
+	);
 
 	return (
 		<FormItem>
@@ -36,8 +60,9 @@ export function FormCombobox({
 				aria-invalid={!!error}
 				open={open}
 				onOpenChange={setOpen}
-				value={typeof value === 'string' ? { value, label: value } : value}
-				onValueChange={(val: any) => onChange?.(val?.value)}
+				multiple={multiple as any}
+				value={comboboxValue as any}
+				onValueChange={handleValueChange}
 				{...props}
 			/>
 			{!!description && <FormDescription>{description}</FormDescription>}
